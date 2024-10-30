424234c144504b7d526b7d2a6590ab90c544ea27
박성원 <skfhddlg@naver.com>
2024-10-30 08:47:59 +0000

    extend

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 
 Invoke(() => Console.WriteLine("Hello from C# delegate!"));
 Wrapper.Invoke(() => Console.WriteLine("Hello from C# delegate! 2"));
+Wrapper.Invoke2(i => Console.WriteLine($"Hello from C# delegate! {i}"), 3);
+Console.WriteLine($"Hello from C# delegate! {Wrapper.Invoke3(() => 4)}");
 
 [DllImport("csharpcallbackgolang.so", CallingConvention = CallingConvention.Cdecl)]
 static extern void Invoke(Action callback);
@@ -11,4 +13,12 @@ static partial class Wrapper
     [LibraryImport("csharpcallbackgolang.so")]
     //[UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
     public static partial void Invoke(Action callback);
+
+    [LibraryImport("csharpcallbackgolang.so")]
+    public static partial void Invoke2(Action2 callback, int i);
+    public delegate void Action2(int i);
+
+    [LibraryImport("csharpcallbackgolang.so")]
+    public static partial int Invoke3(Funci callback);
+    public delegate int Funci();
 }
--- main.go
+++ main.go
@@ -3,6 +3,12 @@ package main
 /*
 typedef void (*Action)();
 static void invoke(Action callback) { callback(); }
+
+typedef void (*Action2)(int i);
+static void invoke2(Action2 callback, int i) { callback(i); }
+
+typedef int (*Func)();
+static int invoke3(Func callback) { return callback(); }
 */
 import "C"
 
@@ -11,4 +17,14 @@ func main() {}
 //export Invoke
 func Invoke(callback C.Action) {
 	C.invoke(callback)
+}
+
+//export Invoke2
+func Invoke2(callback C.Action2, i C.int) {
+	C.invoke2(callback, i)
+}
+
+//export Invoke3
+func Invoke3(callback C.Func) C.int {
+	return C.invoke3(callback)
 }
